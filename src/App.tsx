0367cd8709c4142e344d2ac5b6c00f4592a78f39
rovsen-24 <rnuryiev@gmail.com>
2023-04-10 00:42:52 +0500

    starter kit ready

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,5 @@ import { useState } from "react";
 
 export default function App() {
   const [count, setCount] = useState(0);
-  return <div>Some</div>
+  return <div className="text-green-900 text-2xl">Some text</div>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./tailwind.css"
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
